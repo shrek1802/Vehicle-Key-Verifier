e2f3aefa4dc33e83ab4a3f59d5eaad0901cf04f1
shrek1802 <47456335+shrek1802@users.noreply.github.com>
2026-07-20 21:53:32 +0100

    Update vehicle_detail_screen.dart

--- lib/screens/vehicle_detail_screen.dart
+++ lib/screens/vehicle_detail_screen.dart
@@ -1,1 +1,14 @@
-1
+import 'package:flutter/material.dart';
+
+class VehicleDetailScreen extends StatelessWidget {
+  const VehicleDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Vehicle Detail Screen'),
+      ),
+    );
+  }
+}
