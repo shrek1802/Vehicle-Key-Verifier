79916433b403115bab1524c56a2d263dbfbd6472
shrek1802 <47456335+shrek1802@users.noreply.github.com>
2026-07-20 21:29:13 +0100

    Add VehicleScreen with tab navigation
    Implemented VehicleScreen with a TabController for managing tabs.

---
+++ lib/screens/vehicle_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class VehicleScreen extends StatefulWidget {
+  const VehicleScreen({super.key});
+
+  @override
+  State<VehicleScreen> createState() => _VehicleScreenState();
+}
+
+class _VehicleScreenState extends State<VehicleScreen>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(
+      length: 5,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
